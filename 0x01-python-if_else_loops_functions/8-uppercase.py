9a2fe75d05b03674892a7757ecf6b1abfc60f0f7
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-01-06 05:42:47 -0800

    8. To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) in range(ord('a'), ord('z') + 1):
-            print("{}".format(chr(ord(c) - 32)), end='')
-            continue
-        print("{}".format((c)), end='')
+        print("{}".format(chr(ord(c) - 32)) if ord(c) /
+              in range(ord('a'), ord('z') + 1) else c, end='')
+    print("".format())

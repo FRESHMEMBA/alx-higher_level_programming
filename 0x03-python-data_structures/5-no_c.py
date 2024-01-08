09767a166f9e3c9c1c9197e701f648b53711bc56
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-01-08 13:31:40 -0800

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     no_c_string = "";
     for char in my_string:
-        if char not in ['c', 'C']:
+        if char != 'c' and char != 'C':
             no_c_string += char
     return no_c_string

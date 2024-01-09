f797807144d2c941e597e34595228658e0d9e772
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-01-09 12:55:39 -0800

    11. Delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if idx in range(len(my_list)):
-        temp_list = []
-        for i in range(len(my_list)):
-            if i != idx:
-                temp_list.append(my_list[i])
-        my_list = [item for item in temp_list]
+        del my_list[idx]
     return my_list

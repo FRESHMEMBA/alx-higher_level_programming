0c817b8a303669a41c5fc82768e5368e1453543f
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-01-10 11:35:37 -0800

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 import hidden_4
 
 if __name__ == "__main__":
-    names = sorted([name for name in dir(hidden_4) if not name.startswith("__")])
-    for name in names:
+    names = [name for name in dir(hidden_4) if not name.startswith("__")]
+    for name in sorted(names):
         print(name)
 

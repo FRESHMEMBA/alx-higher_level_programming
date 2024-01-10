013da04cf9d26ffb8f8541079f35ba922369f636
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-01-10 13:08:51 -0800

    6. Build my own calculator!

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    operator = ""
+
+    if len(sys.argv) != 4:
+        print(f"Usage: {sys.argv[0]} <a> <operator> <b>")
+        print(1)
+        exit(1)
+    elif sys.argv[2] not in "+-*/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        print(1)
+        exit(1)
+    else:
+        operator = sys.argv[2]
+        a, b = int(sys.argv[1]), int(sys.argv[3])
+
+        if operator == '+':
+            result = add(a, b)
+        elif operator == '-':
+            result = sub(a, b)
+        elif operator == '*':
+            result = mul(a, b)
+        else:
+            result = div(a, b)
+        
+        print(f"{a} {operator} {b} = {result}")
+        print(0)
+        exit(0)

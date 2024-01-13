72ab1bad168473799dc040787be43205f9ee0b14
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-01-13 16:47:50 +0200

    Multiple files in one commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [n**2 for n in matrix]
+    matrix_squared = []
+    for row in matrix:
+        row_squared = []
+        for n in row:
+            row_squared.append(n**2)
+        matrix_squared.append(row_squared)
+    return matrix_squared

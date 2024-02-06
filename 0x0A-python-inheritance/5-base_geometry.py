381173bcabf1ed5df6ba37cc977bc8c39eae4c23
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-02-06 05:44:34 +0200

    5. Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Defines an empty class called BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """
+    This is the BaseGeometry class.
+
+    It serves as a base class for other geometry classes.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """

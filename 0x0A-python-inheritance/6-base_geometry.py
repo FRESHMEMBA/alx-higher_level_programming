e16a8856031dc1c5a876bd33e2f3ba1f2b37ab0a
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-02-07 00:18:46 +0200

    6. Improve geometry

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -20,7 +20,8 @@ class BaseGeometry():
         """
         Calculate the area of the geometry.
 
-        This method is not implemented in the BaseGeometry class and should be overridden in the derived classes.
+        This method is not implemented in the BaseGeometry class and should be
+        overridden in the derived classes.
 
         Parameters:
             self (BaseGeometry): The instance of the BaseGeometry class.

5b814907cd792155c1c039e9ef33d591f4364e4f
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-02-06 05:01:53 +0200

    0. Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,10 @@
 #!/user/bin/python3
-"""Defines a funtion that returns a list of available attributes and methods
-   of an object.
 """
+Defines a funtion that returns a list of available
+attributes and methods of an object.
+"""
+
+
 def lookup(obj):
     """
     Returns a list of available attributes and methods of an object.

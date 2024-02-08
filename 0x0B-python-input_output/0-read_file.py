e2a07f251d3e786ff5119350669dcf04c4459429
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-02-08 03:05:38 +0200

    0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Defines a function that reads a file and print its content
+on the standard output.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads the content of a file and prints it.
+
+    Parameters:
+    - filename (str): The name of the file to be read.
+        If no filename is provided, an empty string is used.
+
+    Returns:
+    - None
+
+    Raises:
+    - FileNotFoundError: If the specified file does not exist.
+    """
+    with open(filename, encoding="utf-8") as file:
+        content = file.read()
+        print(content)

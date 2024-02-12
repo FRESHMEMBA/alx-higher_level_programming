cf17d5162b176c577fe84a0032b6a8ba2f34d0b6
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-02-12 03:54:47 -0800

    2. First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -19,3 +19,56 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+
+    @property
+    def width(self):
+        """
+        Getter method for the width attribute
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter method for the width attribute
+        """
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Getter method for the height attribute
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        Getter method for the x attribute
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        Setter method for the x attribute
+        """
+        self.__x = value
+
+    @property
+    def y(self)
+    """
+    Getter method for the y attribute
+    """
+    return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        Setter method for the y attribute
+        """
+        self.__y = value

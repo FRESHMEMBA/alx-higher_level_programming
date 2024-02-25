7aa76c296f661713a44bead01139145a91ba047e
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-02-26 01:23:51 +0200

    8. Compare rectangles

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -115,7 +115,7 @@ class Rectangle:
         """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        
+
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
 

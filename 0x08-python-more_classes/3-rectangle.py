81630be8d37e2e9f6c6c06580b1c17929231495e
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-02-25 22:41:17 +0200

    3. String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -83,7 +83,7 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return 0
         for _ in range(self.height):
-            print('#' * self.width, end='')
+            print('#' * self.width)
         return ""
 
     def __repr__(self):

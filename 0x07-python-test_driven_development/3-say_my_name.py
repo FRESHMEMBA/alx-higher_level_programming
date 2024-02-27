cfe2ebc7fc9e8f3967979157d1f306af5d12d160
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-02-27 16:46:05 +0200

    2. Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -10,7 +10,8 @@ def say_my_name(first_name, last_name=""):
 
     Parameters:
         first_name (str): The first name of the person.
-        last_name (str, optional): The last name of the person. Defaults to an empty string.
+        last_name (str, optional): The last name of the person.
+        Defaults to an empty string.
 
     Raises:
         TypeError: If the first_name or last_name is not a string.
@@ -20,7 +21,7 @@ def say_my_name(first_name, last_name=""):
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    
+
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 

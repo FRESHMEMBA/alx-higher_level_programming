eaa7abde22980180e59f13f8bb0e801ea58c6667
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-02-27 16:07:39 +0200

    1. Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,14 +9,18 @@ def matrix_divided(matrix, div):
     Divides all the elements of a matrix by a given divisor.
 
     Parameters:
-    - matrix (list of lists): The matrix to be divided. Each element of the matrix should be an integer or a float.
+    - matrix (list of lists): The matrix to be divided. Each element of the
+    matrix should be an integer or a float.
     - div (int or float): The divisor to divide the matrix elements by.
 
     Returns:
-    - matrix_div (list of lists): The resulting matrix after dividing each element by the divisor. Each element of the resulting matrix will be rounded to 2 decimal places.
+    - matrix_div (list of lists): The resulting matrix after dividing each
+    element by the divisor. Each element of the resulting matrix will be
+    rounded to 2 decimal places.
 
     Raises:
-    - TypeError: If the matrix is not a list of lists, or if any element of the matrix is not an integer or a float.
+    - TypeError: If the matrix is not a list of lists, or if any element
+    of the matrix is not an integer or a float.
     - TypeError: If the rows of the matrix do not have the same size.
     - ZeroDivisionError: If the divisor is zero.
     """
@@ -36,14 +40,14 @@ def matrix_divided(matrix, div):
         if not isinstance(row, list):
             raise TypeError(
                 "matrix must be a matrix (list of lists) of integers/floats"
-            )
+                )
 
         matrix_row = []
         for n in row:
             if not isinstance(n, (int, float)):
                 raise TypeError(
                 "matrix must be a matrix (list of lists) of integers/floats"
-            )
+                )
 
             try:
                 matrix_row.append(round(n / div, 2))

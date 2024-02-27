8f7ae8012530846b96049ae3b695e1bf7a5449e3
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-02-27 16:42:43 +0200

    2. Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
 """
+Defines a function that prints a person's name.
 """
 
+
 def say_my_name(first_name, last_name=""):
+    """
+    Prints a person's name.
+
+    Parameters:
+        first_name (str): The first name of the person.
+        last_name (str, optional): The last name of the person. Defaults to an empty string.
+
+    Raises:
+        TypeError: If the first_name or last_name is not a string.
+
+    Returns:
+        None
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
+
     print("My name is {} {}".format(first_name, last_name))

ba4dcd73bbc9ccc29cf2e4095c126a4e4de0b810
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-02-27 11:37:32 +0200

    30. Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -9,7 +9,7 @@ class LockedClass:
     Class LockedClass prevents the user from dynamically creating new instance
     attributes, except if the new attribute is called first_name
     """
-    __slots__ = ["first_name"]
+    # __slots__ = ["first_name"]
     def __setattr__(self, name, value):
         """
         Overrides the setattr function

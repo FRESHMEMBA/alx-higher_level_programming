26d068341e7416163d9e463539125a38d212a60e
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-02-27 20:50:33 +0200

    4. Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -18,5 +18,9 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     
-    for c in text:
-        print(c, end='' if c not in ".?:" else "\n\n")
+    for i in range(len(text)):
+        if text[i] in ".?:":
+            print(text[i], end="\n")
+            i += 1
+            continue
+        print(text[i], end='')

1ec1d93bd72ce9a5c9f1e122ad56f475e0b00192
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-02-27 12:47:06 +0200

    Renaming the Tests directory to tests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 """
+Defines a function named add_integer that adds two integers
 """
 
 def add_integer(a, b=98):
     """
+    Returns the sum of two numbers, a and b.
+    
+    Parameters:
+    a (int or float): The first number to be added.
+    b (int or float, optional): The second number to be added. Defaults to 98.
+    
+    Returns:
+    int: The sum of a and b.
+    
+    Raises:
+    TypeError: If a or b is not an integer or a float.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")

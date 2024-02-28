d9ba533fccc471b0b8c7fa3d23e21f625021922b
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-02-28 07:57:56 +0200

    7. Lazy matrix multiplication

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -22,5 +22,5 @@ def lazy_matrix_mul(m_a, m_b):
         This function uses the numpy library to perform matrix multiplication
         The input matrices are converted to numpy arrays before multiplication
     """
-    # return np.array(m_a) @ np.array(m_b)
-    return np.dot(m_a, m_b)
+    return np.array(m_a) @ np.array(m_b)
+    # return np.dot(m_a, m_b)

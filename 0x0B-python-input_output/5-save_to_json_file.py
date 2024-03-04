1d90f9462c53aa4b345ec9770cf6dec84738ed57
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-03-04 23:59:40 +0200

    5. Save object to a file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -26,5 +26,5 @@ def save_to_json_file(my_obj, filename):
     Example:
     save_to_json_file({"name": "John", "age": 30}, "data.json")
     """
-    with open(filename) as file:
+    with open(filename, 'w') as file:
         json.dump(my_obj, file)

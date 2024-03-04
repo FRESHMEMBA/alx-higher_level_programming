a723195d9637126fdee83c8a7d57d3bdb719dddc
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-03-05 00:27:44 +0200

    6. Create Object from a JSON file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,5 +8,22 @@ import json
 
 
 def load_from_json(filename):
+    """
+    Loads data from a JSON file and returns it as a Python object.
+
+    Parameters:
+    - filename (str): The name of the JSON file to load.
+
+    Returns:
+    - object: The Python object representing the data in the JSON file.
+
+    Raises:
+    - FileNotFoundError: If the specified file does not exist.
+    - JSONDecodeError: If the file contains invalid JSON data.
+
+    Example:
+        >>> load_from_json('data.json')
+        {'name': 'John', 'age': 30, 'city': 'New York'}
+    """
     with open(filename) as file:
         return json.load(file)

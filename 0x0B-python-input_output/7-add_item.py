c2cf1287d8d1f0ad7c72c37ee9b2462a26ff74d7
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-03-05 03:10:06 +0200

    7. Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """
-Script to add all arguments to a Python list and then save them to a JSON file
-"""
+Script to add all command-line arguments to a Python list and
+then save them to a JSON file.
 
+This script takes command-line arguments and stores them in a Python list.
+It then saves this list to a JSON file for later use.
+
+Usage:
+    python3 script.py [arg1] [arg2] ...
+
+    - Replace [arg1], [arg2], ... with the arguments you
+    want to add to the list.
+
+Dependencies:
+    This script requires Python 3.x to run.
+
+Note:
+    Before running this script, ensure that the '5-save_to_json_file.py'
+    and '6-load_from_json_file.py' modules are correctly implemented,
+    as they are imported to handle JSON file I/O.
+"""
 
 import sys
 import json
@@ -12,7 +29,8 @@ load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file
 
 
 def main():
-    """Main function to process command-line arguments
+    """
+    Main function to process command-line arguments
     and save to JSON file
     """
     # Define the filename for the JSON file

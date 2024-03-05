3fcee53b2bd3bed99d749e32b5c69b9afbc9570d
Sibonelo Mazibuko <123705498+FRESHMEMBA@users.noreply.github.com>
2024-03-05 17:31:35 +0200

    8. Class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Defines a function that returns the dictionary description with simple data
+structure for JSON serialization of an object
+"""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description for JSON serialization
+    """
+    attributes = obj.__dict__
+    json_dict = {}
+
+    for key, value in attributes.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            json_dict[key] = value
+    
+    return json_dict

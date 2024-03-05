8e3b9bd9ca1d50a3b2160da70db922eeb1454a08
Sibonelo Mazibuko <123705498+FRESHMEMBA@users.noreply.github.com>
2024-03-05 17:55:26 +0200

    10. Student to json with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Defines a class that defines a student
+"""
+
+
+class Student:
+    """
+    A class that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Constructor method for the student class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary represenatation of a Student instance
+        """
+        attributes = self.__dict__
+        json_dict = {}
+
+        for key, value in attributes.items():
+            if isinstance(value, (list, dict, str, int, bool)):
+                json_dict[key] = value
+
+        return json_dict

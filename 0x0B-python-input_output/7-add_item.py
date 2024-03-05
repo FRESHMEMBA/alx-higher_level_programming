b7e3796b26a349a3659b42cb72b7a8f5d0d8aed6
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-03-05 02:34:44 +0200

    7. Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,6 +10,7 @@ import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file
 
+# def save_arguments(filename):
 filename = "add_item.json"
 args_list = []
 

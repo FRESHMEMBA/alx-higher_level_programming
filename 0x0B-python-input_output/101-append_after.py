db2b1c4a63fb57303216335fd9e37cd66beb21c5
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-03-06 13:04:24 +0200

    101. Search and update

---
+++ 0x0B-python-input_output/101-append_after.py
@@ -0,0 +1,23 @@
+#!/usr/bin/pyrhon3
+"""
+Defines a funtion that appends a line of text after
+a specific line in the file
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file, after each line containing a specific
+    string.
+    """
+    with open(filename) as file:
+        lines = file.readlines()
+
+    updated_lines = []
+    for line in lines:
+        updated_lines.append(line)
+        if search_string in line:
+            updated_lines.append(new_string + '\n')
+
+    with open(filename, 'w') as file:
+        file.writelines(updated_lines)

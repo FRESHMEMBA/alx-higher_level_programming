c9cc82b34cee631ba661f284d20874440bb067c6
Sibonelo Mazibuko <smazibuko023@student.wethinkcode.co.za>
2024-03-06 11:18:36 +0200

    11. Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Defines a class that defines a student
+"""
+
+
+class Student:
+    """
+    A class that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Constructor method for the student class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary represenatation of a Student instance
+        """
+        types = (list, dict, str, int, bool)
+        attributes = self.__dict__
+        json_dict = {}
+
+        if attrs is None:
+            for key, value in attributes.items():
+                if isinstance(value, types):
+                    json_dict[key] = value
+        elif isinstance(attrs, list):
+            for attr in attrs:
+                if attr in attributes and isinstance(attributes[attr], types):
+                    json_dict[attr] = attributes[attr]
+        return json_dict
+
+    def reload_from_json(self, json_data):
+        """
+        Replaces all attributes of the Student instance
+        """
+        for key, value in json_data.items():
+            setattr(self, key, value)

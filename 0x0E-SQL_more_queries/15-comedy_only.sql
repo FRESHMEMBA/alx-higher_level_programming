8806513e5c3c1f7b523e33b7f637411feec5b577
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-04-08 01:15:40 +0200

    15. Only comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
--- Uses the hbtn_0d_tvshows database to list all genres of the show Dexter.
+-- Lists all comedy shows
 SELECT tv_shows.title
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = "Comedy"
 ORDER BY tv_shows.title ASC;

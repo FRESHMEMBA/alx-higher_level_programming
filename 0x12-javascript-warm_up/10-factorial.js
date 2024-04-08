fd0d82a26ba940c4e41bf358968908877e22faec
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-04-09 00:26:16 +0200

    10. Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 function fact (n) {
   if (isNaN(parseInt(n))) {
-  return 1;
+    return 1;
   } else if (n === 0) {
-  return 1;
+    return 1;
   } else {
-  return n * fact(n - 1);
+    return n * fact(n - 1);
   }
 }
 
-const args = process.argv
-console.log(fact(args[2]))
+const args = process.argv;
+console.log(fact(args[2]));

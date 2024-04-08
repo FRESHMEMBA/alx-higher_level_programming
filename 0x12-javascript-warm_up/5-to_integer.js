fecca8711fb9fa7511bc80eb6de07d38c71b9122
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-04-08 22:51:51 +0200

    5. An integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const args = process.argv;
 const parsedVar = parseInt(args[2]);
 if (isNaN(parsedVar)) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log('My number:', parsedVar);
+  console.log('My number:', parsedVar);
 }

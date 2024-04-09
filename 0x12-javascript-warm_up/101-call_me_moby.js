03d70da061dccb57b8bd3b54ef516949a01449b5
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-04-09 02:54:43 +0200

    15. Call me Moby

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-function callMeMoby(x, func) {
-    for (let i = 0; i < x; i++) {
-        func();
-    }
+function callMeMoby (x, func) {
+  for (let i = 0; i < x; i++) {
+    func();
+  }
 }
 
 module.exports.callMeMoby = callMeMoby;

b2bac33ef98ab870e75e64350b516b49e47f12fd
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-04-10 03:29:42 +0200

    13. Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,4 +4,4 @@ const fs = require('fs');
 const file1 = fs.readFileSync(args[2], 'utf-8');
 const file2 = fs.readFileSync(args[3], 'utf-8');
 const file3 = file1 + file2;
-fs.writeFile(args[4], file3);
+fs.writeFileSync(args[4], file3);

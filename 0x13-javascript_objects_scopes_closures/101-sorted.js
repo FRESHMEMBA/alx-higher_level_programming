a9c950c0417f2cab3f26277590e9170c96d1e86b
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-04-10 02:48:24 +0200

    12. Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,11 +2,11 @@
 const dict = require('./101-data').dict;
 const newDict = {};
 for (const [userId, occurences] of Object.entries(dict)) {
-    if (!newDict[occurences]) {
-        newDict[occurences] = [];
-    };
+  if (!newDict[occurences]) {
+    newDict[occurences] = [];
+  }
 
-    newDict[occurences].push(userId);
-};
+  newDict[occurences].push(userId);
+}
 
 console.log(newDict);

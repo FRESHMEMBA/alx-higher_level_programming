c52fe676ea1ffdda6eacbd624c87de1d5a0b1603
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-04-10 02:14:48 +0200

    11. Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 console.log(list);
-console.log(list.map((x) => x * list.indexOf(x)));
-// console.log(newList);
+// const newList = list.map((x) => x * list.indexOf(x));
+const newList = list.map((item, index) => item * index);
+console.log(newList);

6df9a42d1693682d86473da96e5358c2d7a1451b
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-04-25 23:41:20 +0200

    0. cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
-# Takes in a URL, sends a request to that URL,
-# and displays the response's body size 
-URL=$1
-curl -s -w "%{size_download}\n" "http://$URL"
+# Takes in a URL, sends a request to that URL, and displays the response's body size 
+curl -s -w "%{size_download}\n" -o /dev/null "http://$1"

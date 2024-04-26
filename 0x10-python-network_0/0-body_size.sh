f50abcb2f78388b8fcd48e0669e73115dc7bb78b
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-04-26 13:24:56 +0200

    0. cURL bosy size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a request to that URL, and displays the response's body size 
+# Takes in a URL, sends a request to that URL
 curl -s -w "%{size_download}\n" -o /dev/null "http://$1"

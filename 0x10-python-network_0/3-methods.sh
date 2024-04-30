a0f4871ac7b1cc2e3237fa4ab3474cb5ee3813ab
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-05-01 00:12:32 +0200

    3. cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, and displays all the HTTP methods the server will accept
-curl -s OPTIONS "http://$1" | grep "Allow:"
+curl -s -X OPTIONS "http://$1" | grep "Allow:" | awk '{print $2'}

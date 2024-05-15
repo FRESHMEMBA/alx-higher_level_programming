3cbd1188bd47a8124ab0d63d6a83d42629a646bd
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-05-15 09:49:38 +0200

    Removed the following directories: 0x0F-python-object_relational_mapping, 0x10-python-network_0, 0x12-javascript-warm_up, and 0x13-javascript_objects_scopes_closures

--- 0x10-python-network_0/0-body_size.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Takes in a URL, sends a request to that URL
-curl -s -w "%{size_download}\n" -o /dev/null "http://$1"
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const args = process.argv;
-const parsedVar = parseInt(args[2]);
-if (isNaN(parsedVar)) {
-  console.log('Not a number');
-} else {
-  console.log('My number:', parsedVar);
-}

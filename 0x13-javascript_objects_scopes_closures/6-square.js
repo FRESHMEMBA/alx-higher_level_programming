6aacc406b8ac002a03dffc05fc9a307f7e15ed7e
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-06-20 02:25:38 +0200

    0x13. JavaScript - Objects, Scopes and Closures_objects_scopes_closures

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const SquareB = require('./5-square');
+
+class Square extends SquareB {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (typeof c === 'undefined') {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write(c);
+        }
+        console.log();
+      }
+    }
+  }
+}
+
+module.exports = Square;

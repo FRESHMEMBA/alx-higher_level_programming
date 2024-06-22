615cf26ae44d680970fb2a5e40a42f7ba97a47d3
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-06-23 00:45:38 +0200

    orm

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Defines a function that filters all states with a name starting with N
+"""
+
+
+import sys
+import MySQLdb
+
+
+def filter_states(username, password, database):
+    """
+    Filters all states with a name staring with N from a database
+    """
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT states FROM * WHERE name LIKE 'N%' ORDER BY id ASC"
+        )
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    filter_states(username, password, database)

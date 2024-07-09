e4e3d7f464cf66adc798a3213769dba4808dd224
Sibonelo Mazibuko <123705498+FRESHMEMBA@users.noreply.github.com>
2024-07-09 10:48:01 +0000

    6. POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+"""
+Takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter, and displays the body of the
+response (decoded in utf-8)
+"""
+
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {
+        "email": email,
+    }
+
+    # make a post request to the server
+    response = requests.post(url=url, data=data)
+
+    # print the body response
+    print(f"Your email is: {response.text}")

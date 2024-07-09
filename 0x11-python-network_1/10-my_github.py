12513faa6b1902d7ed7d2f775aa287fcd4f7bc1d
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-07-10 01:51:02 +0200

    9. My GitHub

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+"""
+Takes GitHub credentials and uses the GitHub API
+to display the (user) id
+"""
+
+
+import sys
+import requests
+import requests.auth
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = "https://api.github.com/user"
+
+    response = requests.get(
+        url=url,
+        auth=requests.auth.HTTPBasicAuth(
+            username=username,
+            password=password
+            )
+        )
+    
+    print(response.json()["id"])

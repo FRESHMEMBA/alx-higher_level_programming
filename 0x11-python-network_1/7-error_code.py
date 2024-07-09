354487f3408d3e2b5b6eb21a1926cdc339f677a6
Sibonelo Mazibuko <123705498+FRESHMEMBA@users.noreply.github.com>
2024-07-09 10:54:38 +0000

    7. Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+"""
+Takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8).
+"""
+
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        response = requests.get(url=url)
+        print(f"{response.text}")
+
+    except requests.HTTPError as e:
+        print(f"Error code: {e.code}")

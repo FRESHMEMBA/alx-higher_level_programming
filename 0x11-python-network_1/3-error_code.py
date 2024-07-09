89631e76a7daeb313fad7a94f3410700c0ac6d92
Sibonelo Mazibuko <123705498+FRESHMEMBA@users.noreply.github.com>
2024-07-09 10:15:01 +0000

    3. Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+"""
+Takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8).
+"""
+
+
+import sys
+from urllib import request, error
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with request.urlopen(url) as response:
+            body = response.read().decode("utf-8")
+            print(f"{body}")
+
+    except error.HTTPError as e:
+        print(f"Error code: {e.code}")

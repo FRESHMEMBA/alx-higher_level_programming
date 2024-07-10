f5a4903387913ddf37c2715402f066bba9122045
Sibonelo Mazibuko <123705498+FRESHMEMBA@users.noreply.github.com>
2024-07-10 13:31:16 +0000

    debug 2-post_email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -27,4 +27,4 @@ if __name__ == "__main__":
 
     with request.urlopen(req) as response:
         body = response.read().decode("utf-8")
-        print(f"Your email is: {body}")
+        print(f"{body}")
